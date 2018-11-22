6e32f47f607bc0392c532b370f06da669b0fdbf3
Krister Jansson <krustur@gmail.com>
2018-11-22 21:53:07 +0100

    verbose builds

--- b.bat
+++ b.bat
@@ -1,4 +1,4 @@
-@echo off
-REM -v for verbose
-vc src/main.c -stack-check -size -lamiga -lposix -IE:\Amiga\KrustWB3\Output\Dev\vbcc_posix\targets\m68k-amigaos\include -LE:\Amiga\KrustWB3\Output\Dev\vbcc_posix\targets\m68k-amigaos\lib -DBUILD_PLATFORM_WIN -o IconAlign
+@REM echo off
+@REM -v for verbose
+vc src/main.c -v -stack-check -size -lamiga -lposix -IE:\Amiga\KrustWB3\Output\Dev\vbcc_posix\targets\m68k-amigaos\include -LE:\Amiga\KrustWB3\Output\Dev\vbcc_posix\targets\m68k-amigaos\lib -DBUILD_PLATFORM_WIN -o IconAlign
 copy IconAlign r
